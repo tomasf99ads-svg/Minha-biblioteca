fbda346bce0e73c167aded70aacf4d0818094f6f
tomasf99ads-svg <tomasf99ads@gmail.com>
2026-09-23 22:28:51 +0100

    Create App.jsx

---
+++ src./src/App.jsx
@@ -0,0 +1,12 @@
+import "./index.css";
+
+function App() {
+  return (
+    <main>
+      <h1>Minha Biblioteca 📚</h1>
+      <p>A tua biblioteca digital está a começar.</p>
+    </main>
+  );
+}
+
+export default App;
